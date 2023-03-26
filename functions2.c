1e8bc11d859f098dc82875d0810fab465895a66c
Asmaa Sherif Ahmed <64560423+asmaa-sherif@users.noreply.github.com>
2023-03-26 21:38:39 +0200

    Update functions2.c

--- functions2.c
+++ functions2.c
@@ -101,7 +101,7 @@ int print_non_printable(va_list types, char buffer[],
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
- * Return: Numbers of chars 
+ * Return: Numbers of chars printed
  */
 
 int print_reverse(va_list types, char buffer[],
